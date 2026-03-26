ee1560394298559fd315ae806edf53d73427adb7
rafaelo240120-ops <rafael.o.240120@gmail.com>
2026-03-26 08:57:02 -0300

    ..

--- estrategia_roe.py
+++ estrategia_roe.py
@@ -2,22 +2,30 @@ import requests
 import pandas as pd
 
 base_url = "https://laboratoriodefinancas.com/api/v2"
-token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzc2NTEyNTA4LCJpYXQiOjE3NzM5MjA1MDgsImp0aSI6IjJhMzc3ZjEwYmY0NjRjOGU5NzFmNTZlZDYwYmE2OWYzIiwidXNlcl9pZCI6Ijk5In0.71YX2ayZ8MOuXfjOW0z8zWGP13W8NjyUZjKGsa3VNf0"
+token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzc3MTE4MDM4LCJpYXQiOjE3NzQ1MjYwMzgsImp0aSI6ImI3YWY2NjZiOWIxZjQzYjlhMmI2ZmVhOGFiOGM0ZmVkIiwidXNlcl9pZCI6Ijk5In0.gBQfgMUQxuFm0kh-s5YAYbGKRhazQK3Hl-DVYarMCqM"
 resp = requests.get(
     f"{base_url}/bolsa/planilhao",
     headers={"Authorization": f"Bearer {token}"},
-    params={"data_base": "2026-03-23"},
+    params={"data_base": "2025-03-23"},
 )
 dados =resp.json()
 df = pd.DataFrame(dados)
-maximo = df["roe"].max()
-filtro = df["roe"] == maximo
-df[filtro]
+df2 = df[["ticker", "roe", "p_vp"]]
+df2 = df2.assign(rank_roe=df2["roe"].rank(ascending=False))
+df2 = df2.assign(rank_p_vp=df2["p_vp"].rank(ascending=True))
+df2 = df2.assign(rank_final=df2["rank_roe"] + df2["rank_p_vp"])
+
+
+print(type(dados))
+print(dados)
+
+df2 = df2.sort_values("rank_final", ascending=False)
+
 
 import requests
 
 base_url = "https://laboratoriodefinancas.com/api/v2"
-token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzc2NTEyNTA4LCJpYXQiOjE3NzM5MjA1MDgsImp0aSI6IjJhMzc3ZjEwYmY0NjRjOGU5NzFmNTZlZDYwYmE2OWYzIiwidXNlcl9pZCI6Ijk5In0.71YX2ayZ8MOuXfjOW0z8zWGP13W8NjyUZjKGsa3VNf0"
+token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzc3MTE4MDM4LCJpYXQiOjE3NzQ1MjYwMzgsImp0aSI6ImI3YWY2NjZiOWIxZjQzYjlhMmI2ZmVhOGFiOGM0ZmVkIiwidXNlcl9pZCI6Ijk5In0.gBQfgMUQxuFm0kh-s5YAYbGKRhazQK3Hl-DVYarMCqM"
 params = {"ticker": "MNPR3", "data_ini": "2025-03-21", "data_fim": "2026-03-23"}
 resp = requests.get(
     f"{base_url}/preco/corrigido",
@@ -28,42 +36,23 @@ dados = resp.json()
 df_preco = pd.DataFrame(dados)
 
 #Rendimento
-#Jeito 1
-df_preco["rendimento"] = df_preco["fechamento"].pct_change() * 100
-df_preco["rendimento_acumulado"] = (1 + df_preco["rendimento"] / 100).cumprod() - 1
-df_preco["rendimento_acumulado"] = df_preco["rendimento_acumulado"] * 100
-
-df_preco[["data", "fechamento", "rendimento", "rendimento_acumulado"]]
-
-#Jeito 2
-
-filtro = df_preco["data"] == "2026-03-23"
-preco_final = df_preco[filtro]["fechamento"].iloc[0]
-filtro = df_preco["data"] == "2025-03-21"
-preco_inicial = df_preco[filtro]["fechamento"].iloc[0]
-rendimento = (preco_final / preco_inicial - 1) * 100
-rendimento
-
-#ibovespa
-
-import requests
-
-base_url = "https://laboratoriodefinancas.com/api/v2"
-token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzc2NTEyNTA4LCJpYXQiOjE3NzM5MjA1MDgsImp0aSI6IjJhMzc3ZjEwYmY0NjRjOGU5NzFmNTZlZDYwYmE2OWYzIiwidXNlcl9pZCI6Ijk5In0.71YX2ayZ8MOuXfjOW0z8zWGP13W8NjyUZjKGsa3VNf0"
-
-params = {"ticker": "ibov", "data_ini": "2025-03-21", "data_fim": "2026-03-23"}
-resp = requests.get(
-    f"{base_url}/preco/diversos",
-    headers={"Authorization": f"Bearer {token}"},
-    params=params,
-)
-dados = resp.json()
-df_ibov = pd.DataFrame(dados)
-filtro1 = df_ibov["data"] == "2026-03-20"
-preco_final = df_ibov[filtro1]["fechamento"].iloc[0]
-preco_final = float(preco_final)
-filtro2 = df_ibov["data"] == "2025-06-02"
-preco_inicial = df_ibov[filtro2]["fechamento"].iloc[0]
+filtro1 = df_preco["data"] == "2026-03-23"
+preco_final = df_preco[filtro1]["fechamento"].iloc[0]
+precos_final = float(preco_final)
+filtro2 = df_preco["data"] == "2025-03-21"
+preco_inicial = df_preco[filtro2]["fechamento"].iloc[0]
 preco_inicial = float(preco_inicial)
+(preco_final / preco_inicial - 1) * 100
 
-preco_final/preco_inicial-1
+#API ibovespa
+import yfinance as yf
+#get ticker data
+ibov = yf.download("^BVSP", start="2025-03-21")
+filtro1 = ibov.index == "2025-03-21"
+ibov_inicial = ibov[filtro1]["Close"].iloc[0]
+filtro2 = ibov.index == "2026-03-23"
+ibov_final = ibov[filtro2]["Close"].iloc[0]
+(ibov_final/ibov_inicial-1) * 100 
+
+
+
